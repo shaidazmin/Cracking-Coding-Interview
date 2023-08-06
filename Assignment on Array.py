5148764ba0e4978eeca66341427bf14b843b210f
Shaid Azmin <excel.azmin@gmail.com>
2023-08-06 10:15:52 +0600

    complete Assignment on Array

---
+++ Assignment on Array.py
@@ -0,0 +1,20 @@
+class Solutions:
+    def maxProfit(self,prices):
+        if not prices:
+            return 0
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+print(Solutions().maxProfit([7,6,4,3,1]))
+
+# time complexity is O(n) 
+# space complexity is O(1)
